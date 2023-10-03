6405c3033ad71d51371124d6464ad7189147b114
Arya Radinsyah Fauzi <radinsyah.fauzi@gmail.com>
2023-10-03 09:23:53 -0700

    membuat burger navbar

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
+burger = document.querySelector(".burger");
+burger.onclick = function() {
+    navbar = document.querySelector(".navbar-section")
+    navbar.classList.toggle("active")
+}
+
+
+
+
 const mengetik = new Typed(".typing", {
-    strings: ["Welcome to My portofolio website. 👋"],
-    typeSpeed: 100,
+    strings: ["Welcome to My portofolio website. "],
+    typeSpeed: 50,
+    backSpeed: 10,
     loop: false,
+    showCursor: false,
 });

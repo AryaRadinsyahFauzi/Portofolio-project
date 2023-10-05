c18f1ebd1b27417152de96728ae25eef03c06580
Arya Radinsyah Fauzi <radinsyah.fauzi@gmail.com>
2023-10-05 05:29:24 -0700

    update link sosmed

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
 burger = document.querySelector(".burger");
-burger.onclick = function() {
+burger.onclick = function () {
     navbar = document.querySelector(".navbar-section")
     navbar.classList.toggle("active")
 }
 
-
-const mengetik = new Typed(".typing", {
-    strings: ["Welcome to My portofolio website. 👋 "],
-    typeSpeed: 50,
-    backSpeed: 10,
-    loop: false,
-    showCursor: false,
-});
-
-const ngetik = new Typed(".type" , {
+const ngetik = new Typed(".type", {
     strings: ["Software Engineer", "Data Analyst"],
     typeSpeed: 100,
     backSpeed: 50,
@@ -21,3 +12,30 @@ const ngetik = new Typed(".type" , {
     showCursor: false,
 });
 
+const mengetik = new Typed(".typewriter", {
+    strings: ["Welcome to My portofolio website. 👋 "],
+    typeSpeed: 50,
+    backSpeed: 10,
+    loop: false,
+    showCursor: false,
+});
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('nyala');
+                document.querySelector('header nav a  [href*= '+ id +']').classList.add('nyala');
+            });
+        };
+    });
+};
